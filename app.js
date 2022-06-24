76afb056211022a1136de23c7f38be67ac4f23ea
MGF <m.g.fedai@gmail.com>
2022-06-24 15:20:37 +0300

    NodeMon initialization

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function(){
-    console.log('Function Working');
-};
+const express = require("express");
+
+const app = express();
+app.get('/',(req,res)=>{
+
+    const photo = {
+        id:1,
+        name :'photo name',
+        description:'Photo Description'
+    }
+    res.send(photo)
+})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda başlatıldı`);
+});
